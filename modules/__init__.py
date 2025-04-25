f848418c2f37d56458f1534a408638cc3c4787f4
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2025-04-26 01:52:24 +0530

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py for AnshiChatRobot modules
+
+# Importing all necessary modules for easy access
+from .ping import ping_command
+from .alive import alive_command
+from .start import start_command
+from .help import help_command
+from .translate import translate_command
+from .AnandAi import AnandAi_command
+from .chatbot import chatbot_command
